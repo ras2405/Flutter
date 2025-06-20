17108442a2f34bbed50c937d9d7702f77deacf81
Romina <rominar2000@gmail.com>
2025-06-20 12:51:51 -0300

    phase 1.1 tests ok

--- fase1_fundamentos_flutter_y_dart/01_dart_basics/ejercicios/challenge.dart
+++ fase1_fundamentos_flutter_y_dart/01_dart_basics/ejercicios/challenge.dart
@@ -21,27 +21,82 @@
 
 import 'dart:async';
 
-//TODO: Declara una excepción custom llamada InvalidUsernameException que extienda Exception.
+// Declara una excepción custom llamada InvalidUsernameException que extienda Exception.
+class InvalidUsernameException implements Exception {
+  final String message;
 
-//TODO: Declara un enum UserRole con dos valores: admin y normal.
+  InvalidUsernameException(this.message);
 
-//TODO: Declara una clase Usuario con las propiedades: name (String), age (int), hobbies (List<String>), role (UserRole).
+  @override
+  String toString() => 'InvalidUsernameException: $message';
+}
 
+// Declara un enum UserRole con dos valores: admin y normal.
+enum UserRole { admin, normal }
+
+// Declara una clase Usuario con las propiedades: name (String), age (int),
+// hobbies (List<String>), role (UserRole).
+class Usuario {
+  String name;
+  int age;
+  List<String> hobbies;
+  UserRole role;
+  int? bonus;
+
+  Usuario({
+    required this.name,
+    required this.age,
+    required this.hobbies,
+    required this.role,
+    required this.bonus,
+  });
+}
 
 /// Simula una consulta asincrónica a una base de datos ficticia.
 Future<Usuario> getUserProfile(String username) async {
-  // TODO: Verifica que el username no sea vacío. Si lo es, lanza InvalidUsernameException.
+  // Verifica que el username no sea vacío. Si lo es, lanza InvalidUsernameException.
+  if (username.isEmpty) {
+    throw InvalidUsernameException(
+      'El nombre de usuario no puede estar vacío.',
+    );
+  }
 
-  // TODO: Simula un delay de red usando Future.delayed.
+  // Simula un delay de red usando Future.delayed.
+  await Future.delayed(const Duration(milliseconds: 500));
 
-  // TODO: Determina el rol según el username.
+  // Determina el rol según el username.
+  late UserRole currentRole;
+  // Usando un if para determinar el rol del usuario
+  if (username == 'admin') {
+    currentRole = UserRole.admin;
+  } else {
+    currentRole = UserRole.normal;
+  }
 
-  // TODO: Crea y retorna un Usuario ficticio.
+  // Crea y retorna un Usuario ficticio.
+  return Usuario(
+    name:
+        username[0].toUpperCase() +
+        username.substring(1), // primera letra del nombre en mayus
+    age: currentRole == UserRole.admin ? 40 : 24, // admin: edad 40, si no 24
+    hobbies: ['Leer', 'Programar'],
+    role: currentRole, // role asignado en el if anterior
+    bonus: currentRole == UserRole.admin ? 1000 : null, // bonus solo para admin
+  );
 }
 
 /// Calcula el salario según el rol del usuario. Usa null safety en bonus.
 int calculateSalary(Usuario user) {
-  //TODO: Usa algun switch para evaluar el rol del usuario y calcular el salario.
+  // Usa algun switch para evaluar el rol del usuario y calcular el salario.
+  late int salary;
+  switch (user.role) {
+    case UserRole.admin:
+      salary = user.age * 1000; // Si es admin, salario = edad * 1000
+    case UserRole.normal:
+      salary = user.age * 500; // Si no es admin, salario = edad * 500
+  }
+  salary += user.bonus ?? 0; // Agrega bonus si existe, sino suma 0
+  return salary;
 }
 
 void main() async {
