445e7cd61b640e49cf832b898afe9734840f0bd1
Romina <rominar2000@gmail.com>
2025-06-21 15:12:06 -0300

    phase 1.2 diagnostico widget tree ok

--- project/fase1_fundamentos_flutter_y_dart/02_widget_tree_y_ciclo_de_vida/ejercicios/diagnostico_widget_tree.dart
+++ project/fase1_fundamentos_flutter_y_dart/02_widget_tree_y_ciclo_de_vida/ejercicios/diagnostico_widget_tree.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 
-
-/// TODO Consigna:
+/// Consigna:
 //   1.  Identifica el error relacionado con el uso del BuildContext.
 //   2.  Explica por qué es riesgoso.
 //   3.  Corrige el código respetando buenas prácticas de ciclo de vida.
-
 
 void main() {
   runApp(const MaterialApp(home: DiagnosticPage()));
@@ -26,11 +24,27 @@ class _DiagnosticPageState extends State<DiagnosticPage> {
       _loading = true;
     });
 
+    final BuildContext context = this.context;
+
     await Future.delayed(const Duration(seconds: 3));
 
-    ScaffoldMessenger.of(context).showSnackBar(
-      const SnackBar(content: Text('¡Operación completada!')),
-    );
+    // **Error**:
+    // ScaffoldMessenger.of(context).showSnackBar(
+    //   const SnackBar(content: Text('¡Operación completada!')),
+    // );
+    // Usar context después de un await (como al usar un Future Method en este caso)
+    // tiene el riesgo de estar apuntando a un contexto que fue reconstruido o disposed,
+    // puede filtrar memoria y causar errores (app crashes por ejemplo).
+    // Referencia: https://medium.com/nerd-for-tech/do-not-use-buildcontext-in-async-gaps-why-and-how-to-handle-flutter-context-correctly-870b924eb42e
+
+    // **Corrección**:
+    if (context.mounted) {
+      // 'mounted' usado para verificar si context sigue activo
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(const SnackBar(content: Text('¡Operación completada!')));
+    }
+    // Referencia: https://dart.dev/tools/diagnostics/use_build_context_synchronously
 
     setState(() {
       _loading = false;
@@ -45,9 +59,9 @@ class _DiagnosticPageState extends State<DiagnosticPage> {
         child: _loading
             ? const CircularProgressIndicator()
             : ElevatedButton(
-          onPressed: _simulateAsyncAction,
-          child: const Text('Ejecutar acción async'),
-        ),
+                onPressed: _simulateAsyncAction,
+                child: const Text('Ejecutar acción async'),
+              ),
       ),
     );
   }
